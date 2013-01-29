f5541024d4f03f1347980fd1efff520efa973012
hishidama <hishi.dama@asahi.email.ne.jp>
2013-01-29 21:41:31 +0900

    アウトラインのツリーをダブルクリックで開閉できるよう修正

--- src/jp/hishidama/eclipse_plugin/dmdl_editor/editors/outline/OutlinePage.java
+++ src/jp/hishidama/eclipse_plugin/dmdl_editor/editors/outline/OutlinePage.java
@@ -1,9 +1,14 @@
 package jp.hishidama.eclipse_plugin.dmdl_editor.editors.outline;
 
+import java.util.List;
+
 import jp.hishidama.eclipse_plugin.dmdl_editor.editors.DMDLDocument;
 import jp.hishidama.eclipse_plugin.dmdl_editor.editors.DMDLEditor;
 import jp.hishidama.eclipse_plugin.dmdl_editor.parser.token.ModelList;
 
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
@@ -25,6 +30,22 @@ public class OutlinePage extends ContentOutlinePage {
 		viewer.setLabelProvider(new DMDLLabelProvider());
 		viewer.addSelectionChangedListener(new OutlineSelectionChangedListener(
 				editor));
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				IStructuredSelection selection = (IStructuredSelection) event
+						.getSelection();
+				if (selection.isEmpty()) {
+					return;
+				}
+				List<?> list = selection.toList();
+				TreeViewer viewer = (TreeViewer) event.getSource();
+				for (Object element : list) {
+					boolean old = viewer.getExpandedState(element);
+					viewer.setExpandedState(element, !old);
+				}
+			}
+		});
 		viewer.setInput(root);
 
 		refresh();

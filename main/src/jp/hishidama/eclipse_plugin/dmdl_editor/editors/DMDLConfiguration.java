9bfd9a3233d95e48965e099c85d9a7d8c02eba76
hishidama <hishi.dama@asahi.email.ne.jp>
2013-02-11 21:04:01 +0900

    エラーアイコンにマウスを当てた時にメッセージがポップアップ表示されるよう修正

--- main/src/jp/hishidama/eclipse_plugin/dmdl_editor/editors/DMDLConfiguration.java
+++ main/src/jp/hishidama/eclipse_plugin/dmdl_editor/editors/DMDLConfiguration.java
@@ -19,6 +19,8 @@ import org.eclipse.jface.text.formatter.IContentFormatter;
 import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
+import org.eclipse.jface.text.source.DefaultAnnotationHover;
+import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
 
@@ -117,5 +119,10 @@ public class DMDLConfiguration extends SourceViewerConfiguration {
 	@Override
 	public IContentFormatter getContentFormatter(ISourceViewer sourceViewer) {
 		return new DMDLContentFormatter();
+	}
+
+	@Override
+	public IAnnotationHover getAnnotationHover(ISourceViewer sourceViewer) {
+		return new DefaultAnnotationHover();
 	}
 }
